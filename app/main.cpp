f6ed8af6e4864d1ee22465d84734a1d4bb9e9b62
Erik Strand <erik.strand@cba.mit.edu>
2018-09-30 21:02:38 -0400

    Add supersampling (effectively anti-aliasing)

--- app/main.cpp
+++ app/main.cpp
@@ -13,23 +13,32 @@ void render(Kernel::Tree tree, char const* filename) {
     auto y0 = -10.0;
     auto x1 = 10.0;
     auto y1 = 10.0;
-    auto n_pixels_per_axis = 500;
+    auto n_pixels_per_axis = 100;
+    auto n_samples_per_pixel_per_axis = 3;
+    float n_samples_per_pixel = n_samples_per_pixel_per_axis * n_samples_per_pixel_per_axis;
+    auto n_samples_per_axis = n_pixels_per_axis * n_samples_per_pixel_per_axis;
 
     PngWriter png_writer;
     png_writer.Allocate(n_pixels_per_axis, n_pixels_per_axis);
     png_writer.SetAllPixelsBlack();
 
-    auto dx = (x1 - x0) / n_pixels_per_axis;
-    auto dy = (y1 - y0) / n_pixels_per_axis;
+    auto dx = (x1 - x0) / n_samples_per_axis;
+    auto dy = (y1 - y0) / n_samples_per_axis;
     for (auto i = 0; i < n_pixels_per_axis; ++i) {
-        auto x = x0 + (i + 0.5) * dx;
         for (auto j = 0; j < n_pixels_per_axis; ++j) {
-            auto y = y0 + (j + 0.5) * dy;
-            auto value = e.eval({x, y, 0.5});
-            if (value < 0) {
-                png_writer.SetPixel(i, j);
+            auto n_inside = 0;
+            for (auto k = 0; k < n_samples_per_pixel_per_axis; ++k) {
+                for (auto l = 0; l < n_samples_per_pixel_per_axis; ++l) {
+                    auto x = x0 + (i * n_samples_per_pixel_per_axis + k + 0.5) * dx;
+                    auto y = y0 + (j * n_samples_per_pixel_per_axis + l + 0.5) * dy;
+                    auto value = e.eval({x, y, 0.0});
+                    if (value < 0) {
+                        ++n_inside;
+                    }
+                }
             }
-            //std::cout << x << ", " << y << ": " << value << '\n';
+            uint8_t brightness = static_cast<float>(n_inside) / n_samples_per_pixel  * 255;
+            png_writer.SetPixel(i, j, brightness);
         }
     }
     
@@ -39,7 +48,7 @@ void render(Kernel::Tree tree, char const* filename) {
 
 int main() {
     auto gear_params = GearParams{};
-    gear_params.inputs.module = 1.5;
+    gear_params.inputs.module = 1.2;
     gear_params.inputs.addendum = 1;
     gear_params.inputs.dedendum = 1.1;
     gear_params.inputs.pressure_angle = 0.349066;
